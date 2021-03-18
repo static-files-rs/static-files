07bf6f9c87a8870887196cfa7f0be04342166066
Alexander Korolev <alexander.korolev.germany@gmail.com>
2021-03-18 21:17:35 +0100

    The library to help automate static resource collection

---
+++ README.md
@@ -0,0 +1,16 @@
+# static-files description
+
+## Legal
+
+Dual-licensed under `MIT` or the [UNLICENSE](http://unlicense.org/).
+
+## Features
+
+## Usage
+
+Add dependency to Cargo.toml:
+
+```toml
+[dependencies]
+static-files = "0.1"
+```
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+/*!
+# static-files description
+
+## Features
+
+## Usage
+
+Add dependency to Cargo.toml:
+
+```toml
+[dependencies]
+static-files = "0.1"
+```
+
+*/
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
