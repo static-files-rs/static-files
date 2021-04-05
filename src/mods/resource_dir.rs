91dc16f23c97b8b867378eededf742e9472b51a4
Alexander Korolev <alexander.korolev.germany@gmail.com>
2021-04-05 17:29:59 +0200

    switched default generation to modules

--- src/mods/resource_dir.rs
+++ src/mods/resource_dir.rs
@@ -1,4 +1,4 @@
-use crate::mods::resource::generate_resources;
+use super::sets::{generate_resources_sets, SplitByCount};
 use std::{
     env, io,
     path::{Path, PathBuf},
@@ -33,26 +33,36 @@ pub struct ResourceDir {
     pub(crate) filter: Option<fn(p: &Path) -> bool>,
     pub(crate) generated_filename: Option<PathBuf>,
     pub(crate) generated_fn: Option<String>,
+    pub(crate) module_name: Option<String>,
+    pub(crate) count_per_module: Option<usize>,
 }
+
+pub const DEFAULT_MODULE_NAME: &str = "sets";
+pub const DEFAULT_COUNT_PER_MODULE: usize = 256;
 
 impl ResourceDir {
     /// Generates resources for current configuration.
-    pub fn build(&self) -> io::Result<()> {
-        let generated_filename = self.generated_filename.clone().unwrap_or_else(|| {
+    pub fn build(self) -> io::Result<()> {
+        let generated_filename = self.generated_filename.unwrap_or_else(|| {
             let out_dir = env::var("OUT_DIR").unwrap();
 
             Path::new(&out_dir).join("generated.rs")
         });
-        let generated_fn = self
-            .generated_fn
-            .clone()
-            .unwrap_or_else(|| "generate".into());
+        let generated_fn = self.generated_fn.unwrap_or_else(|| "generate".into());
 
-        generate_resources(
+        let module_name = self
+            .module_name
+            .unwrap_or_else(|| format!("{}_{}", &generated_fn, DEFAULT_MODULE_NAME));
+
+        let count_per_module = self.count_per_module.unwrap_or(DEFAULT_COUNT_PER_MODULE);
+
+        generate_resources_sets(
             &self.resource_dir,
             self.filter,
             &generated_filename,
+            module_name.as_str(),
             &generated_fn,
+            &mut SplitByCount::new(count_per_module),
         )
     }
 
@@ -69,8 +79,29 @@ impl ResourceDir {
     }
 
     /// Sets the generated function name.
-    pub fn with_generated_fn(&mut self, generated_fn: impl Into<String>) -> &mut Self {
+    pub fn with_generated_fn<S>(&mut self, generated_fn: S) -> &mut Self
+    where
+        S: Into<String>,
+    {
         self.generated_fn = Some(generated_fn.into());
+        self
+    }
+
+    /// Sets the generated module name.
+    ///
+    /// Default value is based on generated function name and the suffix "sets".
+    /// Generated module would be overriden by each call.
+    pub fn with_module_name<S>(&mut self, module_name: S) -> &mut Self
+    where
+        S: Into<String>,
+    {
+        self.module_name = Some(module_name.into());
+        self
+    }
+
+    /// Sets maximal count of files per module.
+    pub fn with_count_per_module(&mut self, count_per_module: usize) -> &mut Self {
+        self.count_per_module = Some(count_per_module);
         self
     }
 }
